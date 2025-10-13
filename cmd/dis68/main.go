03d9c72e07b32745988013d19afad2b7079c6917
Urethramancer <github@grimdork.net>
2025-10-13 16:57:41 +0200

    Refactored.

--- cmd/dis68/main.go
+++ cmd/dis68/main.go
@@ -7,42 +7,6 @@ import (
 	"github.com/Urethramancer/m68k/disassembler"
 )
 
-// hexdump prints data in the style of the 'hexdump -C' command.
-func hexdump(data []byte) {
-	const bytesPerLine = 16
-	for i := 0; i < len(data); i += bytesPerLine {
-		// Print the offset for the current line.
-		fmt.Printf("%08x  ", i)
-
-		// Print the hex values for the bytes in the line.
-		for j := 0; j < bytesPerLine; j++ {
-			if j == 8 {
-				fmt.Print(" ") // Add an extra space in the middle.
-			}
-			if i+j < len(data) {
-				fmt.Printf("%02x ", data[i+j])
-			} else {
-				fmt.Print("   ") // Pad with spaces if the line is short.
-			}
-		}
-
-		// Print the ASCII representation.
-		fmt.Print(" |")
-		end := i + bytesPerLine
-		if end > len(data) {
-			end = len(data)
-		}
-		for _, b := range data[i:end] {
-			if b >= 32 && b <= 126 {
-				fmt.Printf("%c", b)
-			} else {
-				fmt.Print(".") // Use a dot for non-printable characters.
-			}
-		}
-		fmt.Println("|")
-	}
-}
-
 func main() {
 	if len(os.Args) < 2 || len(os.Args) > 3 {
 		fmt.Fprintf(os.Stderr, "Usage: %s <inputfile> [outputfile]\n", os.Args[0])
@@ -77,5 +41,5 @@ func main() {
 	}
 
 	// If no output file is specified, print a hexdump of the input binary to the console.
-	hexdump(code)
+	disassembler.Hexdump(code)
 }
