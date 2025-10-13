2b9bbf569b78abfb3cab7ae83123dccb44557fcc
Urethramancer <github@grimdork.net>
2025-10-13 17:15:10 +0200

    Refactor: - Added support for multiple source files - Switched to new HexDump() in the disassembler package

--- cmd/asm68/main.go
+++ cmd/asm68/main.go
@@ -5,46 +5,86 @@ import (
 	"os"
 
 	"github.com/Urethramancer/m68k/assembler"
+	"github.com/Urethramancer/m68k/disassembler"
+	"github.com/grimdork/climate/arg"
+	"github.com/grimdork/climate/str"
 )
 
 func main() {
-	if len(os.Args) < 2 || len(os.Args) > 3 {
-		fmt.Fprintf(os.Stderr, "Usage: %s <sourcefile> [outputfile]\n", os.Args[0])
-		os.Exit(1)
-	}
-
-	var fn string
-	if len(os.Args) == 3 {
-		fn = os.Args[2]
-	}
-
-	src, err := os.ReadFile(os.Args[1])
+	opt := arg.New("asm68")
+	opt.SetDefaultHelp(true)
+	err := opt.SetPositional("SOURCE", "Source file(s) to assemble. They will be assembled in the order listed.", "", true, arg.VarStringSlice)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading source file: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error setting positional argument: %v\n", err)
 		os.Exit(1)
 	}
 
+	err = opt.SetOption(arg.GroupDefault, "o", "out", "Binary output file (default: stdout)", "", false, arg.VarString, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error setting option: %v\n", err)
+		os.Exit(1)
+	}
+
+	err = opt.Parse(os.Args[1:])
+	if err != nil {
+		if err == arg.ErrNoArgs {
+			opt.PrintHelp()
+			return
+
+		}
+
+		fmt.Fprintf(os.Stderr, "Error parsing arguments: %v\n", err)
+		os.Exit(1)
+	}
+
+	files := opt.GetPosStringSlice("SOURCE")
+	// List the files to be assembled.
+	if len(files) == 0 {
+		fmt.Fprintf(os.Stderr, "No source files specified.\n")
+		os.Exit(1)
+	}
+
+	src := str.NewStringer()
+	var count int
+	for _, fn := range files {
+		data, err := os.ReadFile(fn)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading source file: %v\n", err)
+			os.Exit(1)
+		}
+
+		n, err := src.Write(data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error processing source file: %v\n", err)
+			os.Exit(1)
+		}
+
+		count += n
+		// Add a newline between files to avoid accidental token merging.
+		_, err = src.WriteString("\n")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error processing source file: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	fmt.Printf("Read %d bytes of source code.\n", count)
 	asm := assembler.New()
-	code, err := asm.Assemble(string(src), 0)
+	code, err := asm.Assemble(string(src.String()), 0)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Assembly error: %v\n", err)
 		os.Exit(1)
 	}
 
-	if fn == "" {
-		// Print as hex dump for inspection
-		for i, b := range code {
-			fmt.Printf("%02X ", b)
-			if (i+1)%16 == 0 {
-				fmt.Println()
-			}
-		}
-		fmt.Println()
-	} else {
+	fn := opt.GetString("out")
+	if fn != "" {
 		if err := os.WriteFile(fn, code, 0644); err != nil {
 			fmt.Fprintf(os.Stderr, "Error writing output file: %v\n", err)
 			os.Exit(1)
 		}
 		fmt.Printf("Assembled binary written in M68K big-endian format to %s\n", fn)
+		return
 	}
+
+	disassembler.Hexdump(code)
 }
